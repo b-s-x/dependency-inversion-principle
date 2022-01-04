b0b5c4ef69b93b7db5625c39eb369ff3892a8a8a
b-s-x <b.s.x.mars@gmail.com>
2022-01-04 13:58:04 +0300

    @/interfaces/ICustomHttp -> @/interfaces/index: rename

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,10 +3,14 @@ import {
   CustomHttpResponse as Response
 } from "../CustomHttp";
 
-export interface ICustomHttp {
+export interface CustomHttp {
   get(request: Request): Promise<Response>;
   post(request: Request): Promise<Response>;
   put(request: Request): Promise<Response>;
   patch(request: Request): Promise<Response>;
   delete(request: Request): Promise<Response>;
+}
+
+export interface User {
+  username: string;
 }
