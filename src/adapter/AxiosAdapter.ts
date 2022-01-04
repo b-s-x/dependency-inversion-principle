6e58dc7e02ca3b03d62298727ddaa445b269a9f5
b-s-x <b.s.x.mars@gmail.com>
2022-01-04 13:44:08 +0300

    @/adapter/AxiosAdapter: add params

--- src/adapter/AxiosAdapter.ts
+++ src/adapter/AxiosAdapter.ts
@@ -6,30 +6,28 @@ import {
 } from "../CustomHttp";
 
 export class AxiosAdapter implements ICustomHttp {
-  public async get(request: Request, params: object): Promise<Response> {
-    console.log(1, request, params);
-
-    const response = await axios.get(request.path, params);
+  public async get({ path, params }: Request): Promise<Response> {
+    const response = await axios.get(path, { params });
     return new Response(response.data, response.status);
   }
 
-  public async post(request: Request): Promise<Response> {
-    const response = await axios.post(request.path, request.params);
+  public async post({ path, params }: Request): Promise<Response> {
+    const response = await axios.post(path, { params });
     return new Response(response.data, response.status);
   }
 
-  public async put(request: Request): Promise<Response> {
-    const response = await axios.put(request.path, request.params);
+  public async put({ path, params }: Request): Promise<Response> {
+    const response = await axios.put(path, { params });
     return new Response(response.data, response.status);
   }
 
-  public async patch(request: Request): Promise<Response> {
-    const response = await axios.patch(request.path, request.params);
+  public async patch({ path, params }: Request): Promise<Response> {
+    const response = await axios.patch(path, { params });
     return new Response(response.data, response.status);
   }
 
-  public async delete(request: Request): Promise<Response> {
-    const response = await axios.delete(request.path);
+  public async delete({ path, params }: Request): Promise<Response> {
+    const response = await axios.delete(path, { params });
     return new Response(response.data, response.status);
   }
 }
